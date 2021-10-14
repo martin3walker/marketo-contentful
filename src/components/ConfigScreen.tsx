4958a9e165656a0fdbbf413b6bfd630938c7aff2
Martin Walker <martin.walker@contentful.com>
2021-10-14 15:45:57 +0200

    first commit

--- src/components/ConfigScreen.tsx
+++ src/components/ConfigScreen.tsx
@@ -1,9 +1,14 @@
 import React, { useCallback, useState, useEffect } from 'react';
 import { AppExtensionSDK } from '@contentful/app-sdk';
-import { Heading, Form, Workbench, Paragraph } from '@contentful/forma-36-react-components';
+import { Heading, Form, Workbench, Paragraph, TextField, TextFieldProps } from '@contentful/forma-36-react-components';
 import { css } from 'emotion';
 
-export interface AppInstallationParameters {}
+export interface AppInstallationParameters {
+  clientId?: string;
+  clientSecret?: string;
+  munchkinId?: string;
+  [key:string]: string | undefined;
+}
 
 interface ConfigProps {
   sdk: AppExtensionSDK;
@@ -12,52 +17,82 @@ interface ConfigProps {
 const Config = (props: ConfigProps) => {
   const [parameters, setParameters] = useState<AppInstallationParameters>({});
 
-  const onConfigure = useCallback(async () => {
-    // This method will be called when a user clicks on "Install"
-    // or "Save" in the configuration screen.
-    // for more details see https://www.contentful.com/developers/docs/extensibility/ui-extensions/sdk-reference/#register-an-app-configuration-hook
-
-    // Get current the state of EditorInterface and other entities
-    // related to this app installation
+  // Updates app configuration by calling this function as a callback in the app.sdk.onConfigure function
+  const configureApp = useCallback(async () => {
     const currentState = await props.sdk.app.getCurrentState();
-
     return {
-      // Parameters to be persisted as the app configuration.
       parameters,
-      // In case you don't want to submit any update to app
-      // locations, you can just pass the currentState as is
       targetState: currentState,
     };
   }, [parameters, props.sdk]);
 
+  // Called whenever the config changes
   useEffect(() => {
-    // `onConfigure` allows to configure a callback to be
-    // invoked when a user attempts to install the app or update
-    // its configuration.
-    props.sdk.app.onConfigure(() => onConfigure());
-  }, [props.sdk, onConfigure]);
+    props.sdk.app.onConfigure(() => configureApp());
+  }, [props.sdk, configureApp, parameters]);
 
+  // Initial load for config page
   useEffect(() => {
     (async () => {
-      // Get current parameters of the app.
-      // If the app is not installed yet, `parameters` will be `null`.
       const currentParameters: AppInstallationParameters | null = await props.sdk.app.getParameters();
 
+      // Set local parameter state based on the installation params
       if (currentParameters) {
         setParameters(currentParameters);
+      } else {
+        setParameters({
+          clientSecret: "",
+          clientId: "",
+          munchkinId: ""
+        })
       }
 
-      // Once preparation has finished, call `setReady` to hide
-      // the loading screen and present the app to a user.
       props.sdk.app.setReady();
     })()
   }, [props.sdk])
+
+  const handleFieldChange:(
+    event:React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+    ) => void = (event) => {
+      const id  = event.target.id;
+      const value = event.target.value;
+      const newParameters:{[key:string]: any} = {
+        ...parameters
+      }
+      newParameters[id] = value;
+      setParameters(newParameters);
+    }
+
 
   return (
     <Workbench className={css({ margin: '80px' })}>
       <Form>
         <Heading>App Config</Heading>
         <Paragraph>Welcome to your contentful app. This is your config page.</Paragraph>
+        <TextField 
+          required 
+          name="clientId" 
+          id="clientId" 
+          labelText="Marketo Client ID" 
+          onChange={(event) => handleFieldChange(event)} 
+          value={parameters.clientId}
+        />
+        <TextField 
+          required 
+          name="clientSecret" 
+          id="clientSecret" 
+          labelText="Marketo Client Secret" 
+          onChange={(event) => handleFieldChange(event)} 
+          value={parameters.clientSecret}
+        />
+        <TextField 
+          required 
+          name="munchkinId" 
+          id="munchkinId" 
+          labelText="Marketo Munchkin Id" 
+          onChange={(event) => handleFieldChange(event)} 
+          value={parameters.munchkinId}
+        />
       </Form>
     </Workbench>
   );
--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Paragraph } from '@contentful/forma-36-react-components';
-import { FieldExtensionSDK } from '@contentful/app-sdk';
+import { FieldExtensionSDK, AppExtensionSDK } from '@contentful/app-sdk';
+import { requestHandler, otherHandler } from "../functions/getMarketoIds"
 
 interface FieldProps {
   sdk: FieldExtensionSDK;
 }
 
+interface Parameters {
+  clientId: string;
+  munchkinId: string;
+  clientSecret: string;
+}
+
+interface FormObject {
+  title: string;
+  id: string;
+}
+
+
 const Field = (props: FieldProps) => {
+  const [formsArray, updateFormsArray] = useState<FormObject[] | null>(null);
+  console.log(formsArray)
+
+  // useEffect (() => {
+  //   //Get form data from function
+  //    (async () => {
+  //     const formData:FormObject[] = await (await fetch('https://www.contentful.com/.netlify/functions/getMarketoFormIds')).json()
+
+  //     if(formData) {
+  //       updateFormsArray(formData) 
+  //     }
+  //   })();
+  // }, [])
+
+  useEffect(() => {
+    (async () => {
+      const { clientId, clientSecret, munchkinId }:any = props.sdk.parameters.installation
+      const res = requestHandler(clientId, clientSecret, munchkinId)
+      console.log(res)
+    })();
+  }, [props.sdk.parameters.installation])
+
+
   // If you only want to extend Contentful's default editing experience
   // reuse Contentful's editor components
   // -> https://www.contentful.com/developers/docs/extensibility/field-editors/
