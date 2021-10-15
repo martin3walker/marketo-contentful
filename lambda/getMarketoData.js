741260479cb1f7bf10fd32f41fdac3e3ce3f32ad
Martin Walker <martin.walker@contentful.com>
2021-10-15 16:11:12 +0200

    working version

--- lambda/getMarketoData.js
+++ lambda/getMarketoData.js
@@ -12,118 +12,113 @@ const request = ({ method, hostname, path, headers }) =>
       .end();
   });
 
-const getMarketoData = function(clientId, clientSecret, munchkinId) {
-  request({
-    method: "GET",
-    hostname: `${munchkinId}.mktorest.com`,
-    path: `/identity/oauth/token?grant_type=client_credentials&client_id=${
-     clientId
-    }&client_secret=${clientSecret}`
-  })
-    .then(res => res.access_token)
-    .then(bearer =>
-      request({
+const getMarketoData = async (clientId, clientSecret, munchkinId) => {
+// Get Marketo Bearer Token
+  try {
+    const auth = await request(
+      {
+        method: "GET",
+        hostname: `${munchkinId}.mktorest.com`,
+        path: `/identity/oauth/token?grant_type=client_credentials&client_id=${
+        clientId
+        }&client_secret=${clientSecret}`
+      }
+    )
+    //Get forms from Rest Api
+    const response = await request(
+      {
         method: "GET",
         hostname: `${munchkinId}.mktorest.com`,
         path: `/rest/asset/v1/forms.json?maxReturn=200`,
         headers: {
-          Authorization: `Bearer ${bearer}`
+          Authorization: `Bearer ${auth.access_token}`
         }
-      })
-      .then(res => console.log(res))
-        .then(res =>
-          callback(null, {
-            statusCode: 200,
-            headers: {
-              "Access-Control-Allow-Origin": "*",
-              "Content-Type": "application/json"
-            },
-            body: JSON.stringify(res.result)
-          })
-        )
-        .catch(err => {
-          console.log(err);
-          callback(err);
-        })
+      }
     )
-    .catch(err => {
-      console.log(err);
-    });
+    return response;
+
+  } catch(error) {
+    return {
+      error: true,
+      message: error
+    }
+  }
 };
-
-// const getMarketoData = async (clientId, clientSecret, munchkinId) => {
-//   const resp = await fetch(`https://${munchkinId}.mktorest.com/identity/oauth/token?grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`, {
-//     headers: {
-//       "Access-Control-Allow-Origin":'Netlify',
-//     }
-//   })
-//   console.log(resp)
-//   // const res = await fetch(`
-//   //   ${munchkinId}.mktorest.com/rest/asset/v1/forms.json?maxReturn=200`, {
-//   //     method: "GET",
-//   //     headers: {
-//   //       Authorization: `Bearer ${access_token}`,
-//   //       "Access-Control-Allow-Origin": munchkinId,
-//   //       "Content-Type": "application/json"
-//   //   }
-//   // })
-
-//   // const data = await res.body
-//   // console.log(data)
-// }
 
 
 exports.handler =  async (event) => {
+  if (event.body) {
+    const {clientId, clientSecret, munchkinId} = JSON.parse(event.body);
+    if (!clientId){
+      return {
+        statusCode: 400,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "POST",
+          "Access-Control-Allow-Headers": "Content-Type",
+          "Content-Type": "application/json"
+        },
+        body: "Check client id"
+      }
+    } else if (!clientSecret) {
+      return {
+        statusCode: 400,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "POST",
+          "Access-Control-Allow-Headers": "Content-Type",
+          "Content-Type": "application/json"
+        },
+        body: "Check client secret"
+      }
+    } else if (!munchkinId) {
+      return {
+        statusCode: 400,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "POST",
+          "Access-Control-Allow-Headers": "Content-Type",
+          "Content-Type": "application/json"
+        },
+        body: "Check munchkin id"
+      }
+    }
+
+    const marketoData = await getMarketoData(clientId, clientSecret, munchkinId)
+    if (marketoData.error) {
+      return {
+        statusCode: 400,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "POST",
+          "Access-Control-Allow-Headers": "Content-Type",
+          "Content-Type": "application/json"
+        },
+        body: "Bad request to marketo"
+      }
+    } else {
+      const data = JSON.stringify(marketoData)
+      return {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "POST",
+          "Access-Control-Allow-Headers": "Content-Type",
+          "Content-Type": "application/json"
+        },
+        body: data
+      }
+    }
+  }
 
   return {
     statusCode: 200,
-    body: "hi"
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "POST",
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Content-Type": "application/json"
+    },
+    body: ""
   }
-  // if(event.httpMethod !== "POST") {
-  //   return {statusCode: 200, body: "Please visit the main page to estimate the value of your package."}; 
-  // }
-
-  // const payload = JSON.parse(event.body);
-
-  // const {clientId, clientSecret, munchkinId} = payload;
-
-  // const marketoData = await getMarketoData(clientId, clientSecret, munchkinId);
-  // console.log(marketoData);
-    
-  // return {
-  //   statusCode: 200,
-  //   body: JSON.stringify({data: marketoData}),
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //   }
-  // }
 }
-// exports.handler =  async (event) => {
-//   if(event.httpMethod !== "POST") {
-//     return {statusCode: 200, body: "Please visit the main page to estimate the value of your package."}; 
-//   }
-
-//   const payload = JSON.parse(event.body);
-
-//   const {valuation, optionsAmount, strikePrice} = payload;
-
-
-//   const calculateOptionValueUsd = (totalValuation, noOfOptions, strikePrice) => {
-//     const dilution = .1;
-//     const totalShares = 92448944;
-//     const sharePrice = (totalValuation)/(totalShares*(1+dilution));
-//     const valueOfOptionsUsd = Math.max(0, ((sharePrice-strikePrice)*noOfOptions));
-
-//     return valueOfOptionsUsd;
-//   }
-
-//   const value = calculateOptionValueUsd(valuation, optionsAmount, strikePrice);
-    
-//   return {
-//     statusCode: 200,
-//     body: JSON.stringify({value: value}),
-//     headers: {
-//       "Content-Type": "application/json",
-//     }
-//   }
-// }
--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-//import React, {useState, useEffect} from 'react';
-import { Paragraph } from '@contentful/forma-36-react-components';
+import React, {useState, useEffect} from 'react';
+import { Dropdown, DropdownList, DropdownListItem, Flex, Paragraph, Pill, TextInput} from '@contentful/forma-36-react-components';
 import { FieldExtensionSDK } from '@contentful/app-sdk';
-// import { FieldExtensionSDK, AppExtensionSDK } from '@contentful/app-sdk';
+
+import styled from "styled-components";
 
 
 interface FieldProps {
@@ -15,49 +15,129 @@ interface FieldProps {
 //   clientSecret: string;
 // }
 
-// interface FormObject {
-//   title: string;
-//   id: string;
-// }
+interface FormObject {
+  name: string;
+  id: string;
+}
+
+const StyledDropdownContainer = styled.div`
+  width: 100%;
+  > .dropdown {
+    width: 100%;
+    padding: 10px;
+  }
+`
 
 
 const Field = (props: FieldProps) => {
-  // const [formsArray, updateFormsArray] = useState<FormObject[] | null>(null);
+  const [forms, updateForms] = useState<FormObject[] | null>(null);
+  const [selectedForm, updateSelectedForm] = useState<FormObject | null>(null);
+  const [dropdownForms, updateDropdownForms] = useState<FormObject[] | null>(null);
 
-  // useEffect (() => {
-  //   //Get form data from function
-  //    (async () => {
-  //      const res = fetch("http://localhost:9999/.netlify/functions/getMarketoData", {
-  //        method: "POST",
-  //        body: JSON.stringify(props.sdk.parameters.installation),
-  //        headers: {
-  //          'Access-Control-Allow-Origin': '*',
-  //          'Content-Type': 'application/json'
-  //        }
-  //      })
-  //      console.log(res)
-  //     // const formData:FormObject[] = await (await fetch('https://www.contentful.com/.netlify/functions/getMarketoFormIds')).json()
+  props.sdk.window.startAutoResizer()
 
-  //     // if(formData) {
-  //     //   updateFormsArray(formData) 
-  //     // }
-  //   })();
-  // }, [])
+  const updateFieldValue = (item:FormObject) => {
+    if (item.id === selectedForm?.id) {
+      props.sdk.field.setValue(null);
+      updateSelectedForm(null);
+      return;
+    }
+    props.sdk.field.setValue(item);
+    updateSelectedForm(item);
+  }
 
-  // // useEffect(() => {
-  // //   (async () => {
-  // //     const { clientId, clientSecret, munchkinId }:any = props.sdk.parameters.installation
-  // //     console.log(props.sdk.parameters)
-  // //     // const res = requestHandler(clientId, clientSecret, munchkinId)
-  // //     // console.log(res)
-  // //   })();
-  // // }, [props.sdk.parameters.installation])
+  const getDropdownData = (searchTerm:string) => {
+    console.log(searchTerm)
+    if(forms) {
+      const newDropdownData = [...forms].filter((item:FormObject) => {
+        return item.name.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+      })
+  
+      console.log(newDropdownData)
+      return newDropdownData;
+    }
 
+    return null;
+  }
 
-  // // If you only want to extend Contentful's default editing experience
-  // // reuse Contentful's editor components
-  // // -> https://www.contentful.com/developers/docs/extensibility/field-editors/
-  return <Paragraph>Hello Entry Field Component</Paragraph>;
+  useEffect (() => {
+    //Get list of available forms to from Marketo selection
+     (async () => {
+       const response = await ( await fetch("http://localhost:9999/.netlify/functions/getMarketoData", {
+         method: "POST",
+         body: JSON.stringify(props.sdk.parameters.installation),
+         headers: {
+          "Access-Control-Request-Method": "POST",
+          "Content-Type": "application/json"
+         }
+       })).json()
+
+      if(response.result) {
+        updateForms(response.result); 
+        updateDropdownForms(response.result);
+      }
+    })();
+
+    // Set field value in local state
+    const fieldValue = props.sdk.field.getValue()
+    if(fieldValue !== undefined) {
+      updateSelectedForm(fieldValue)
+    }
+
+  }, [props.sdk]) //Think about this
+
+  return (
+    <Flex flexDirection={"column"} fullHeight={true} style={{minHeight: 500}}>
+      {props.sdk.field.getValue() && (
+        <Flex flexDirection={"column"} marginBottom={"spacingL"}>
+          <Paragraph style={{marginBottom: 10}}>
+            Selected form
+          </Paragraph>
+          <Flex flexWrap={"wrap"}>
+            {selectedForm && (
+              <Pill  
+                label={selectedForm.name} 
+                onClose={() => updateFieldValue(selectedForm)} 
+                key={selectedForm.id}
+              />
+            )}
+          </Flex>
+        </Flex>
+      )}
+
+      {dropdownForms && dropdownForms.length > 0 ? (
+        <>
+          <Flex marginBottom={'spacingS'}>
+            <TextInput 
+              onChange={(event) => updateDropdownForms(getDropdownData(event.target.value))} placeholder="Search for a form"
+            />
+          </Flex>
+          <Flex>
+            <StyledDropdownContainer>
+              <Dropdown
+                isOpen={true}
+                dropdownContainerClassName={"dropdown"}
+              >
+                <DropdownList className={"dropdown-list"} maxHeight={500}>
+                  {dropdownForms.map((item) => (
+                    <DropdownListItem 
+                      onClick={() => updateFieldValue(item)} 
+                      isActive={selectedForm ? selectedForm.id === item.id : false} 
+                      key={`key-${item.id}`}
+                    >
+                      {item.name}
+                    </DropdownListItem>
+                  ))}
+                </DropdownList>
+              </Dropdown>
+            </StyledDropdownContainer>
+          </Flex>
+        </>
+      ):(
+        <Paragraph>Try expanding your search</Paragraph>
+      )}
+    </Flex>
+  );
 };
 
 export default Field;
