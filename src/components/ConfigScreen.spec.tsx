7b61755383f182724da5ed9f380ca753291fcf8d
Martin Walker <martin.walker@contentful.com>
2021-10-22 11:13:09 +0200

    more testing work

--- src/components/ConfigScreen.spec.tsx
+++ src/components/ConfigScreen.spec.tsx
@@ -1,17 +1,49 @@
 import React from 'react';
 import ConfigScreen from './ConfigScreen';
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent, configure} from '@testing-library/react';
+import { act } from "react-dom/test-utils";
 import { mockSdk } from '../../test/mocks/mockSdk';
+
+configure({testIdAttribute: 'data-test-id'})
 
 describe('Config Screen component', () => {
   it('Component text exists', async () => {
-    const { getByText } = render(<ConfigScreen sdk={mockSdk} />);
 
+    render(<ConfigScreen sdk={mockSdk} />);
     // simulate the user clicking the install button
-    await mockSdk.app.onConfigure.mock.calls[0][0]();
+    await mockSdk.app.onConfigure.mock.calls[0][0];
 
-    expect(
-      getByText('Welcome to your contentful app. This is your config page.')
-    ).toBeInTheDocument();
+    expect( 
+      screen.getByText('Marketo App Configuration')
+      ).toBeInTheDocument();
+
   });
 });
+
+describe('Config inputs work', () => {
+  it('Values are set correctly', () => {
+
+    render(<ConfigScreen sdk={mockSdk} />);
+
+    screen.getAllByTestId('cf-ui-text-input').forEach((input, i) => {
+      fireEvent.change(input, {
+        target:{ value: `A test value for input ${i}` }
+      })
+    })
+
+    screen.getAllByTestId('cf-ui-text-input').forEach((input, i) => {
+      expect(input).toHaveValue(`A test value for input ${i}`)
+    })
+  });
+});
+
+describe('Test connection button works', () => {
+  it('Produces a notification', () => {
+    render(<ConfigScreen sdk={mockSdk} />);
+
+    const button = screen.getByTestId('cf-ui-button');
+
+    fireEvent.click(button)
+    expect(screen.getByTestId('cf-ui-notification'))
+  })
+})
--- test/mocks/mockSdk.ts
+++ test/mocks/mockSdk.ts
@@ -4,7 +4,22 @@ const mockSdk: any = {
     getParameters: jest.fn().mockReturnValueOnce({}),
     setReady: jest.fn(),
     getCurrentState: jest.fn(),
+    parameters: {
+      installation: {
+        clientSecret: 'testSecret',
+        clientId: 'testId',
+        munchkinId: 'testMunchkin'
+      }
+    }
   },
 };
 
-export { mockSdk };
+const marketoCall: any = jest.fn(() => {
+  return {satus: 200, results:[
+    {name: "Test form 1", id: "1fkdjf"},
+    {name: "Test form 2", id: "1fdfae"},
+    {name: "Test form 3", id: "8sdahf"}
+  ]}
+})
+
+export { mockSdk, marketoCall };
